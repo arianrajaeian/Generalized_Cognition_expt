3cb7c210540a042ceb404eaf89b978ae31496ad3
Arian Rajaeian <arajaeia@asu.edu>
2026-06-26 11:56:19 -0700

    fix code error

--- models.py
+++ models.py
@@ -201,8 +201,8 @@ class RogersAgent(Agent):
         teacher_parent = random.choice(parents)
         A_info = next((a for a in social_info if a.details == "A" and a.origin == teacher_parent), None)
         B_info = next((b for b in social_info if b.details == "B" and b.origin == teacher_parent), None)
-
-        experiment.inherit_social_info(node = self, A_info = A_info, B_info = B_info, parent = teacher_parent)
+        exp = experiment.RogersExperiment()
+        exp.inherit_social_info(node = self, A_info = A_info, B_info = B_info, parent = teacher_parent)
 
 
 
